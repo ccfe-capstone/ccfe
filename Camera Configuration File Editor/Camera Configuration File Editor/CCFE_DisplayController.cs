6f6edeb203c9447937b55a589efb8661e63f341c
gerads <pdgerads@gmail.com>
2016-03-05 22:46:40 -0600

    Changed public variable to property instead.

--- Camera Configuration File Editor/Camera Configuration File Editor/CCFE_DisplayController.cs
+++ Camera Configuration File Editor/Camera Configuration File Editor/CCFE_DisplayController.cs
@@ -9,7 +9,20 @@ namespace Camera_Configuration_File_Editor
     public class CCFE_DisplayController
     {
         CCFE_Configuration configuration;
-        
+
+        public CCFE_Configuration Configuration
+        {
+            get
+            {
+                return configuration;
+            }
+
+            set
+            {
+                configuration = value;
+            }
+        }
+
         public void setupDisplay()
         {
 
