229d541b9f9e6248193f4baf73d47a8be5d5098f
gerads <pdgerads@gmail.com>
2016-03-06 23:12:29 -0600

    Added constructors to support opening existing configuration files and making new ones based on a version number. Added method for updating value of a specific property.

--- Camera Configuration File Editor/Camera Configuration File Editor/CCFE_Configuration.cs
+++ Camera Configuration File Editor/Camera Configuration File Editor/CCFE_Configuration.cs
@@ -31,12 +31,45 @@ namespace Camera_Configuration_File_Editor
         {
             propertyList = new List<CCFE_ConfigurationProperty>();
         }
+
+        public CCFE_Configuration(List<CCFE_ConfigurationProperty> propertyList)
+        {
+            PropertyList = propertyList;
+        }
+
+        public CCFE_Configuration(string configurationVersion)
+        {
+            if (configurationVersion.Equals("1.0"))
+            {
+                //TODO: create properties with default values for configuration version
+                propertyList.Add(new CCFE_ConfigurationProperty("TriggerMode", "5"));
+                propertyList.Add(new CCFE_ConfigurationProperty("OverlapPercent", "75"));
+                propertyList.Add(new CCFE_ConfigurationProperty("KnownHalAltitudeUnits", "feet"));
+                propertyList.Add(new CCFE_ConfigurationProperty("KnownHalAltitude", "400"));
+                propertyList.Add(new CCFE_ConfigurationProperty("Time", "3.8"));
+                propertyList.Add(new CCFE_ConfigurationProperty("Distance", "10"));
+                propertyList.Add(new CCFE_ConfigurationProperty("WaitForGpsFix", "yes"));
+                propertyList.Add(new CCFE_ConfigurationProperty("Version", configurationVersion));
+            }
+        }
         #endregion
 
         #region public methods
         public void addProperty(CCFE_ConfigurationProperty property)
         {
             propertyList.Add(property);
+        }
+
+        public void updateValue(string propertyName, string value)
+        {
+            foreach (CCFE_ConfigurationProperty property in propertyList)
+            {
+                if (property.Name.Equals(propertyName))
+                {
+                    property.Value = value;
+                    return;
+                }
+            }
         }
         #endregion
     }
